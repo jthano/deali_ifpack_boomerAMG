6a8290e79a922f0f6b680314fcecce1eab7d3a37
jthano <josh.hanophy@gmail.com>
2019-02-28 12:25:16 -0800

    Design change: Instead of BoomerAMG_Parameters storing a vector to all the parameters, BoomerAMG_Parameter_Base now has a static vector. The derived class BoomerAMG_Parameter, upon construction, automatically adds a pointer of itself to the this static vector. BoomerAMG_Parameters can then just simply itterate over this vector to set all the parameters

--- BoomerAMG_solver/source/BoomerAMG_solver.cc
+++ BoomerAMG_solver/source/BoomerAMG_solver.cc
@@ -19,15 +19,15 @@ void BoomerAMG_Parameters::pre_post_relax::set_parameter(Hypre_Chooser solver_pr
 		const std::string A("A");
 
 		// Array to store relaxation scheme and pass to Hypre
-		 int **grid_relax_points = (int **) malloc(4*sizeof(int *));
-		 grid_relax_points[0] = NULL;
-		 grid_relax_points[1] = (int *) malloc(sizeof(int)*ns_down);
-		 grid_relax_points[2] = (int *) malloc(sizeof(int)*ns_up);
-		 grid_relax_points[3] = (int *) malloc(sizeof(int));
-		 grid_relax_points[3][0] = 0;
+		int **grid_relax_points = (int **) malloc(4*sizeof(int *));
+		grid_relax_points[0] = NULL;
+		grid_relax_points[1] = (int *) malloc(sizeof(int)*ns_down);
+		grid_relax_points[2] = (int *) malloc(sizeof(int)*ns_up);
+		grid_relax_points[3] = (int *) malloc(sizeof(int));
+		grid_relax_points[3][0] = 0;
 
-		 // set down relax scheme
-		 for(unsigned int i = 0; i<ns_down; i++) {
+		// set down relax scheme
+		for(unsigned int i = 0; i<ns_down; i++) {
 		    if (value.first.compare(i,1,F) == 0) {
 		       grid_relax_points[1][i] = -1;
 		    }
@@ -51,7 +51,8 @@ void BoomerAMG_Parameters::pre_post_relax::set_parameter(Hypre_Chooser solver_pr
 		       grid_relax_points[2][i] = 0;
 		    }
 		 }
-
+		 // Currently some problem with Trilinos I thought I fixed. Here hypre needs an interger pointer to list of intergers
+		 /*
 		FunctionParameter f1 (solver_preconditioner_selection , & HYPRE_BoomerAMGSetGridRelaxPoints , ns_coarse,3);
 		FunctionParameter f2 (solver_preconditioner_selection , & HYPRE_BoomerAMGSetCycleNumSweeps , ns_coarse,3);
 		FunctionParameter f3 (solver_preconditioner_selection , & HYPRE_BoomerAMGSetCycleNumSweeps , ns_down,1);
@@ -60,7 +61,7 @@ void BoomerAMG_Parameters::pre_post_relax::set_parameter(Hypre_Chooser solver_pr
 		parameter_list->push_back(f1);
 		parameter_list->push_back(f2);
 		parameter_list->push_back(f3);
-		parameter_list->push_back(f4);
+		parameter_list->push_back(f4); */
 	}
 
 }
@@ -95,6 +96,92 @@ void BoomerAMG_Parameters::coarsen_type::set_parameter(Hypre_Chooser solver_prec
 
 }
 
+void BoomerAMG_Parameters::print_level::set_parameter(Hypre_Chooser solver_preconditioner_selection, std::vector<FunctionParameter> * parameter_list){
+
+	if (parameter_used)
+	{
+		FunctionParameter function(solver_preconditioner_selection , & HYPRE_BoomerAMGSetPrintLevel , value);
+		parameter_list->push_back(function);
+	}
+
 }
 
+void BoomerAMG_Parameters::max_levels::set_parameter(Hypre_Chooser solver_preconditioner_selection, std::vector<FunctionParameter> * parameter_list){
+
+	if (parameter_used)
+	{
+		FunctionParameter function(solver_preconditioner_selection , & HYPRE_BoomerAMGSetMaxLevels , value);
+		parameter_list->push_back(function);
+	}
+
+}
+
+void BoomerAMG_Parameters::cycle_type::set_parameter(Hypre_Chooser solver_preconditioner_selection, std::vector<FunctionParameter> * parameter_list){
+
+	if (parameter_used)
+	{
+		FunctionParameter function(solver_preconditioner_selection , & HYPRE_BoomerAMGSetCycleType , value);
+		parameter_list->push_back(function);
+	}
+
+}
+
+void BoomerAMG_Parameters::sabs_flag::set_parameter(Hypre_Chooser solver_preconditioner_selection, std::vector<FunctionParameter> * parameter_list){
+
+	if (parameter_used)
+	{
+		FunctionParameter function(solver_preconditioner_selection , & HYPRE_BoomerAMGSetSabs , value);
+		parameter_list->push_back(function);
+	}
+
+}
+
+void BoomerAMG_Parameters::strength_tolC::set_parameter(Hypre_Chooser solver_preconditioner_selection, std::vector<FunctionParameter> * parameter_list){
+
+	if (parameter_used)
+	{
+		FunctionParameter function(solver_preconditioner_selection , & HYPRE_BoomerAMGSetStrongThreshold , value);
+		parameter_list->push_back(function);
+	}
+
+}
+
+void BoomerAMG_Parameters::strength_tolR::set_parameter(Hypre_Chooser solver_preconditioner_selection, std::vector<FunctionParameter> * parameter_list){
+
+	if (parameter_used)
+	{
+		FunctionParameter function(solver_preconditioner_selection , & HYPRE_BoomerAMGSetStrongThresholdR , value);
+		parameter_list->push_back(function);
+	}
+
+}
+
+void BoomerAMG_Parameters::distance_R::set_parameter(Hypre_Chooser solver_preconditioner_selection, std::vector<FunctionParameter> * parameter_list){
+
+	if (parameter_used)
+	{
+		FunctionParameter function(solver_preconditioner_selection , & HYPRE_BoomerAMGSetRestriction , value);
+		parameter_list->push_back(function);
+	}
+
+}
+
+void BoomerAMG_Parameters::filterA_tol::set_parameter(Hypre_Chooser solver_preconditioner_selection, std::vector<FunctionParameter> * parameter_list){
+
+	if (parameter_used)
+	{
+		FunctionParameter function(solver_preconditioner_selection , & HYPRE_BoomerAMGSetADropTol , value);
+		parameter_list->push_back(function);
+	}
+
+}
+
+void BoomerAMG_Parameters::set_parameter_list(std::vector<FunctionParameter> * parameter_vector){
+	for (auto parameter : BoomerAMG_Parameter_Base::parameter_list){
+		parameter->set_parameter( solver_preconditioner_selection, parameter_vector );
+	}
+}
+
+
+} // Close namespace TrilinosWrappers
 DEAL_II_NAMESPACE_CLOSE
--- BoomerAMG_solver/source/BoomerAMG_solver.h
+++ BoomerAMG_solver/source/BoomerAMG_solver.h
@@ -25,7 +25,9 @@ public:
 	 * The primary purpose of this class is to have a single pointer type for each parameter
 	 */
 	class BoomerAMG_Parameter_Base{
+		friend class BoomerAMG_Parameters;
 	protected:
+		static std::vector<BoomerAMG_Parameter_Base *> parameter_list;
 		bool parameter_used=true;
 		virtual void set_parameter(Hypre_Chooser, std::vector<FunctionParameter> *) = 0;
 	};
@@ -36,6 +38,7 @@ public:
 	template<class parameter_type>
 	class BoomerAMG_Parameter :public BoomerAMG_Parameter_Base{
 	public:
+		BoomerAMG_Parameter();
 		parameter_type value;
 	};
 
@@ -54,6 +57,7 @@ public:
 	 * followed by a relexation over C-points.
 	 */
 	class pre_post_relax: public BoomerAMG_Parameter<std::pair<std::string,std::string>>{
+		//pre_post_relax();
 		void set_parameter(Hypre_Chooser, std::vector<FunctionParameter> *);
 	};
 
@@ -79,6 +83,7 @@ public:
 	 * </ul>
 	 */
 	class relax_type : public BoomerAMG_Parameter <int>{
+		//relax_type();
 		void set_parameter(Hypre_Chooser, std::vector<FunctionParameter> *);
 	};
 	/**
@@ -104,6 +109,7 @@ public:
 	 * </ul>
 	 */
 	class interp_type : public BoomerAMG_Parameter <int>{
+		//interp_type();
 		void set_parameter(Hypre_Chooser, std::vector<FunctionParameter> *);
 	};
 	/**
@@ -173,10 +179,6 @@ public:
 		void set_parameter(Hypre_Chooser, std::vector<FunctionParameter> *);
 	};
 
-	class amg_logging : public BoomerAMG_Parameter <int>{
-		void set_parameter(Hypre_Chooser, std::vector<FunctionParameter> *);
-	};
-
 	class strength_tolC : public BoomerAMG_Parameter <double>{
 		void set_parameter(Hypre_Chooser, std::vector<FunctionParameter> *);
 	};
@@ -223,198 +225,14 @@ public:
 
 	void set_parameter_list(std::vector<FunctionParameter> *);
 
-private:
-	/**
-	 * The paramer_list vector stores all the FunctionParameter that will
-	 * be used to set relevant parameters
-	 */
-	std::vector<BoomerAMG_Parameter_Base *> parameter_list;
-
 };
-/*
-class SolverBoomerAMG {
-public:
-	*
-	 * This struct
 
-	struct AdditionalData {
-		explicit AdditionalData(const std::sting prerelax = "A",
-				const std::sting prerelax = "FFFC", const int relax_type = 0,
-				const int interp_type = 100, const int coarsen_type = 6,
-				const int print_level = 3, const int max_iteration = 1000,
-				const int max_levels = 50, const int cycle_type = 1,
-				const int debug_flag = 0, const int sabs_flag = 0,
-				const int trilinos_print_time = 1, const int amg_logging = 0,
-				const double strength_tolC = 5.0e-3,
-				const double strength_tolR = 5.0e-3, const double distance_R =
-						2.0, const double filterA_tol = 1.0e-4,
-				const double solve_tol = 1e-10, const double post_filter_R = 0.0
-
-				);
-		*
-		 * The prerelax string specifies the points, order, and relaxation steps
-		 * for prerelaxation. The options are "A", "F", or "C" where A is relaxation over
-		 * all points, F is relaxation over the F-points, and C is relaxation over the
-		 * C-points. Multiple characters specify multiple relaxation steps and the order
-		 * matters. For example, "AA" specifies two relaxation steps of all points.
-
-		std::string prerelax;
-		*
-		 * The postrelax string specifies the points, order, and relaxation steps
-		 * for postrelaxation. The options are "A", "F", or "C" where A is relaxation over
-		 * all points, F is relaxation over the F-points, and C is relaxation over the
-		 * C-points. Multiple characters specify multiple relaxation steps and the order
-		 * matters. For example, "FFFC" specifies three post relaxations over F-points
-		 * followed by a relexation over C-points.
-
-		std::string postrelax;
-		*
-		 * The relax_type integer variable sets the relaxation type.
-		 * Relaxation types, taken from the hypre documentation, are:
-		 * <ul>
-		 * <li> 0: Jacobi </li>
-		 * <li> 1: Gauss-Seidel, sequential (very slow!) </li>
-		 * <li> 2: Gauss-Seidel, interior points in parallel, boundary sequential (slow!) </li>
-		 * <li> 3: hybrid Gauss-Seidel or SOR, forward solve </li>
-		 * <li> 4: hybrid Gauss-Seidel or SOR, backward solve </li>
-		 * <li> 5: hybrid chaotic Gauss-Seidel (works only with OpenMP) </li>
-		 * <li> 6: hybrid symmetric Gauss-Seidel or SSOR </li>
-		 * <li> 8: \f$\ell_1\f$ Gauss-Seidel, forward solve </li>
-		 * <li> 9: Gaussian elimination (only on coarsest level) </li>
-		 * <li> 13: \f$\ell_1\f$ Gauss-Seidel, forward solve </li>
-		 * <li> 14: \f$\ell_1\f$ Gauss-Seidel, backward solve </li>
-		 * <li> 15: CG (warning - not a fixed smoother - may require FGMRES) </li>
-		 * <li> 16: Chebyshev </li>
-		 * <li> 17: FCF-Jacobi </li>
-		 * <li> 18: \f$\ell_1\f$-scaled jacobi </li>
-		 * </ul>
-
-		int relax_type;
-		*
-		 * The interp_type integer variable sets the interpolation type.
-		 * Interpolation types, taken from the hypre documentation, are:
-		 * <ul>
-		 * <li> 0: classical modified interpolation </li>
-		 * <li> 1: LS interpolation (for use with GSMG) </li>
-		 * <li> 2: classical modified interpolation for hyperbolic PDEs </li>
-		 * <li> 3: direct interpolation (with separation of weights) </li>
-		 * <li> 4: multipass interpolation </li>
-		 * <li> 5: multipass interpolation (with separation of weights) </li>
-		 * <li> 7: extended+i interpolation </li>
-		 * <li> 8: standard interpolation </li>
-		 * <li> 9: standard interpolation (with separation of weights) </li>
-		 * <li> 10: classical block interpolation (for use with nodal systems version only) </li>
-		 * <li> 11: classical block interpolation (for use with nodal systems version only)<br/>
-		 * with diagonalized diagonal blocks<br/></li>
-		 * <li> 12: FF interpolation </li>
-		 * <li> 13: FF1 interpolation </li>
-		 * <li> 14: extended interpolation </li>
-		 * <li> 100: Pointwise interpolation (intended for use with AIR) </li>
-		 * </ul>
-
-		int interp_type;
-		*
-		 * The coarsen_type integer variable sets the coarsening algorithm.
-		 * Coarsening algorithm options, taken from the hypre documentation, are:
-		 * <ul>
-		 * <li> 0: CLJP-coarsening (a parallel coarsening algorithm using independent sets. </li>
-		 * <li> 3: classical Ruge-Stueben coarsening on each processor, followed by a third pass,<br/>
-		 * which adds coarse points on the boundaries <br/></li>
-		 * <li> 6: Falgout coarsening (uses 1 first, followed by CLJP using the interior coarse points<br/>
-		 * generated by 1 as its first independent set) <br/></li>
-		 * <li> 8: PMIS-coarsening (a parallel coarsening algorithm using independent sets, generating<br/>
-		 * lower complexities than CLJP, might also lead to slower convergence) <br/></li>
-		 * <li> 10: HMIS-coarsening (uses one pass Ruge-Stueben on each processor independently, followed<br/>
-		 *  by PMIS using the interior C-points generated as its first independent set) <br/></li>
-		 * <li> 21: CGC coarsening by M. Griebel, B. Metsch and A. Schweitzer </li>
-		 * <li> 22: CGC-E coarsening by M. Griebel, B. Metsch and A.Schweitzer </li>
-		 * </ul>
-
-		int coarsen_type;
-		*
-		 *
-
-		int print_level;
-		*
-		 *
-
-		int max_itter;
-		*
-		 *
-
-		int max_levels;
-		*
-		 * The cycle_type integer variable sets the cycle type. Cycle types available,
-		 * taken from the hypre documentation, are:
-		 * <ul>
-		 * <li> 0: F-cycle type </li>
-		 * <li> 1: V-cycle type </li>
-		 * <li> 2: W-cycle type </li>
-		 * </ul>
-
-		int cycle_type;
-		*
-		 *
-
-		int debug_flag;
-		*
-		 * sabs_flag sets whether the classical strength of connection test
-		 * based on testing the negative of matrix coefficient or if the absolute
-		 * value is tested. If set to 0, the negative coefficient values are tested,
-		 * if set to 1, the absolute values of matrix coefficients are tested.
-
-		int sabs_flag;
-		*
-		 *
-
-		int trilinos_print_time;
-		*
-		 *
-
-		int amg_logging;
-		*
-		 *
-
-		double strength_tolC;
-		*
-		 *
-
-		double strength_tolR;
-		*
-		 * The distance_R double variable sets whether Approximate Ideal Restriction
-		 * (AIR) multigrid or classical multigrid is used.
-		 * <ul>
-		 * <li> 0.0: Use classical AMG, not AIR </li>
-		 * <li> 1.0: Use AIR, Distance-1 LAIR is used to compute R </li>
-		 * <li> 2.0: Use AIR, Distance-2 LAIR is used to compute R </li>
-		 * <li> 3.0: Use AIR, degree 0 Neumann expansion is used to compute R </li>
-		 * <li> 4.0: Use AIR, degree 1 Neumann expansion is used to compute R </li>
-		 * <li> 5.0: Use AIR, degree 2 Neumann expansion is used to compute R </li>
-		 * </ul>
-
-		double distance_R;
-		*
-		 *
-
-		double filterA_tol;
-		*
-		 *
-
-		double solve_tol;
-		*
-		 *
-
-		double post_filter_R;
-	};
-	*
-	 * Constructor. Takes the solver control object and creates the solver.
-
-	SolverBoomerAMG(SolverControl & cn, const AdditionalData &data =
-			AdditionalData());
-
-};
-*/
-
+template<class parameter_type>
+BoomerAMG_Parameters::BoomerAMG_Parameter<parameter_type>::BoomerAMG_Parameter(){
+	parameter_list.push_back(this);
 }
 
+
+
+} // Close namespace TrilinosWrappers
 DEAL_II_NAMESPACE_CLOSE
