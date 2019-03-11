c49c3555fd33b34f4aed5a9a50265eac3ebb5896
jthano <josh.hanophy@gmail.com>
2019-03-11 12:58:10 -0600

    complete design change now relying on boost::variant. The boost::static_visitor class is used to access the variant values in a well controlled manner.

--- BoomerAMG_solver/source/BoomerAMG_solver.cc
+++ BoomerAMG_solver/source/BoomerAMG_solver.cc
@@ -1,12 +1,104 @@
 
-#include<BoomerAMG_solver.h>
+#include <BoomerAMG_solver.h>
 
 DEAL_II_NAMESPACE_OPEN
 
 namespace TrilinosWrappers
 {
 
+BoomerAMG_Parameters::BoomerAMG_Parameters(){
+	/*
+	parameters.insert( {"interp_type", parameter_data(100, & HYPRE_BoomerAMGSetInterpType)} );
+	parameters.insert( {"coarsen_type", parameter_data(6, & HYPRE_BoomerAMGSetCoarsenType)} );
+	parameters.insert( {"relax_type", parameter_data(0, & HYPRE_BoomerAMGSetRelaxType)} );
+	parameters.insert( {"max_hypre_itter", parameter_data(50, & HYPRE_BoomerAMGSetMaxIter)} );
+	parameters.insert( {"max_amg_levels", parameter_data(40, & HYPRE_BoomerAMGSetMaxLevels)} );
+	parameters.insert( {"amg_cycle_type", parameter_data(6, & HYPRE_BoomerAMGSetCycleType)} );
+	parameters.insert( {"sabs_flag", parameter_data(6, & HYPRE_BoomerAMGSetSabs)} );
 
+	parameters.insert( {"distance_R", parameter_data(2.0, & HYPRE_BoomerAMGSetRestriction)} );
+	parameters.insert( {"strength_tolC", parameter_data(2.0, & HYPRE_BoomerAMGSetStrongThreshold)} );
+	parameters.insert( {"strength_tolR", parameter_data(2.0, & HYPRE_BoomerAMGSetStrongThresholdR)} );
+	parameters.insert( {"filterA_tol", parameter_data(2.0, & HYPRE_BoomerAMGSetADropTol)} );
+	parameters.insert( {"post_filter_R", parameter_data(2.0, & HYPRE_BoomerAMGSetFilterThresholdR)} );
+	*/
+
+	parameters.insert( {"hypre_print_level", parameter_data(100, & HYPRE_BoomerAMGSetPrintLevel)} );
+	parameters.insert( {"coarsen_type", parameter_data(6, & HYPRE_BoomerAMGSetCoarsenType)} );
+	parameters.insert( {"relax_type", parameter_data(0, & HYPRE_BoomerAMGSetRelaxType)} );
+	parameters.insert( {"number_sweeps", parameter_data(1, & HYPRE_BoomerAMGSetNumSweeps)} );
+	parameters.insert( {"max_itter", parameter_data(50, & HYPRE_BoomerAMGSetMaxIter)} );
+
+	parameters.insert( {"solve_tol", parameter_data(1e-10, & HYPRE_BoomerAMGSetTol)} );
+
+
+}
+
+void BoomerAMG_Parameters::set_parameters(Ifpack_Hypre & Ifpack_obj, const Hypre_Chooser solver_preconditioner_selection){
+
+	apply_parameter_variant_visitor parameter_visitor(Ifpack_obj, solver_preconditioner_selection);
+
+	for (auto param_itter=parameters.begin();param_itter!=parameters.end();++param_itter){
+		if ((param_itter->second).set_function == nullptr){
+			boost::apply_visitor(parameter_visitor, (param_itter->second).hypre_function, (param_itter->second).value );
+		} else{
+			(param_itter->second).set_function(solver_preconditioner_selection, param_itter->second , Ifpack_obj);
+		}
+	}
+}
+
+void BoomerAMG_Parameters::set_relaxation_order(const Hypre_Chooser solver_preconditioner_selection, const parameter_data & param_data, Ifpack_Hypre & Ifpack_obj){
+
+	std::pair<std::string,std::string> param_value = boost::get< std::pair<std::string,std::string> >(param_data.value);
+
+	const unsigned int ns_down = param_value.first.length();
+	const unsigned int ns_up = param_value.second.length();
+	const unsigned int ns_coarse = 1 ;
+
+	const std::string F("F");
+	const std::string C("C");
+	const std::string A("A");
+
+	// Array to store relaxation scheme and pass to Hypre
+	int **grid_relax_points = (int **) malloc(4*sizeof(int *));
+	grid_relax_points[0] = NULL;
+	grid_relax_points[1] = (int *) malloc(sizeof(int)*ns_down);
+	grid_relax_points[2] = (int *) malloc(sizeof(int)*ns_up);
+	grid_relax_points[3] = (int *) malloc(sizeof(int));
+	grid_relax_points[3][0] = 0;
+
+	// set down relax scheme
+	for(unsigned int i = 0; i<ns_down; i++) {
+	    if (param_value.first.compare(i,1,F) == 0) {
+	       grid_relax_points[1][i] = -1;
+	    }
+	    else if (param_value.first.compare(i,1,C) == 0) {
+	       grid_relax_points[1][i] = 1;
+	    }
+	    else if (param_value.first.compare(i,1,A) == 0) {
+	       grid_relax_points[1][i] = 0;
+	    }
+	 }
+
+	 // set up relax scheme
+	 for(unsigned int i = 0; i<ns_up; i++) {
+	    if (param_value.second.compare(i,1,F) == 0) {
+	       grid_relax_points[2][i] = -1;
+	    }
+	    else if (param_value.second.compare(i,1,C) == 0) {
+	       grid_relax_points[2][i] = 1;
+	    }
+	    else if (param_value.second.compare(i,1,A) == 0) {
+	       grid_relax_points[2][i] = 0;
+	    }
+	 }
+
+	//Ifpack_obj.SetParameter(solver_preconditioner_selection , & HYPRE_BoomerAMGSetGridRelaxPoints , ns_coarse,3);
+	Ifpack_obj.SetParameter(solver_preconditioner_selection , & HYPRE_BoomerAMGSetCycleNumSweeps , ns_coarse,3);
+	Ifpack_obj.SetParameter(solver_preconditioner_selection , & HYPRE_BoomerAMGSetCycleNumSweeps , ns_down,1);
+	Ifpack_obj.SetParameter(solver_preconditioner_selection , & HYPRE_BoomerAMGSetCycleNumSweeps , ns_up,2);
+
+}
 
 }
 DEAL_II_NAMESPACE_CLOSE
--- BoomerAMG_solver/source/BoomerAMG_solver.h
+++ BoomerAMG_solver/source/BoomerAMG_solver.h
@@ -1,15 +1,107 @@
 #include<Ifpack_Hypre.h>
 #include<Epetra_MultiVector.h>
 
+#include <deal.II/lac/generic_linear_algebra.h>
 #include <deal.II/lac/trilinos_vector.h>
 #include <deal.II/lac/solver_control.h>
 #include <deal.II/base/config.h>
+
+#include "boost/variant.hpp"
 
 DEAL_II_NAMESPACE_OPEN
 
 namespace TrilinosWrappers {
 
+namespace LA =  dealii::LinearAlgebraTrilinos::MPI;
 
+class BoomerAMG_Parameters{
+
+public:
+
+	typedef boost::variant<int (*)(HYPRE_Solver, int),int (*)(HYPRE_Solver, double),int (*)(HYPRE_Solver,double, int),
+			int (*)(HYPRE_Solver, int, int),int (*)(HYPRE_Solver, int*),int (*)(HYPRE_Solver, double*),
+			int (*)(HYPRE_Solver, int**),std::nullptr_t> hypre_function_variant;
+
+	typedef boost::variant<int,double,int*,int**,std::pair<double,int>, std::pair<int, int>,
+            std::pair<std::string,std::string>> param_value_variant;
+
+	struct parameter_data{
+		param_value_variant value;
+		hypre_function_variant hypre_function=nullptr;
+		std::function<void(const Hypre_Chooser, const parameter_data &, Ifpack_Hypre &)> set_function=nullptr;
+		parameter_data(param_value_variant value, hypre_function_variant hypre_function):value(value),hypre_function(hypre_function){};
+	};
+
+	BoomerAMG_Parameters();
+
+	enum default_configuration_type{AIR_AMG,CLASSICAL_AMG,NONE};
+
+	void set_parameters(Ifpack_Hypre & Ifpack_obj, const Hypre_Chooser solver_preconditioner_selection);
+
+	std::map< std::string,parameter_data> parameters;
+
+private:
+
+	void set_classical_amg_parameters();
+	void set_air_amg_parameters();
+
+	void set_relaxation_order(const Hypre_Chooser solver_preconditioner_selection, const parameter_data & param_data, Ifpack_Hypre & Ifpack_obj);
+
+	class apply_parameter_variant_visitor:
+			public boost::static_visitor<>
+	{
+	public:
+		apply_parameter_variant_visitor(Ifpack_Hypre & Ifpack_obj, const Hypre_Chooser solver_preconditioner_selection )
+		:Ifpack_obj(Ifpack_obj),solver_preconditioner_selection(solver_preconditioner_selection){};
+
+		void operator()( int (* hypre_set_func)(HYPRE_Solver, int) & , int & value){
+			Ifpack_obj.SetParameter(solver_preconditioner_selection,hypre_set_func,value);
+		}
+
+		void operator()( int (* hypre_set_func)(HYPRE_Solver, double) & , double & value){
+			Ifpack_obj.SetParameter(solver_preconditioner_selection,hypre_set_func,value);
+		}
+
+		void operator()( int (* hypre_set_func)(HYPRE_Solver, double, int) & , std::pair<double,int> & value){
+			Ifpack_obj.SetParameter(solver_preconditioner_selection,hypre_set_func,value.first,value.second);
+		}
+
+		void operator()( int (* hypre_set_func)(HYPRE_Solver, int, int) & , std::pair<int,int> & value){
+			Ifpack_obj.SetParameter(solver_preconditioner_selection,hypre_set_func,value.first,value.second);
+		}
+
+		void operator()( int (* hypre_set_func)(HYPRE_Solver, int*) & , int* & value){
+			Ifpack_obj.SetParameter(solver_preconditioner_selection,hypre_set_func,value);
+		}
+
+		void operator()( int (* hypre_set_func)(HYPRE_Solver, double*) & , double* & value){
+			Ifpack_obj.SetParameter(solver_preconditioner_selection,hypre_set_func,value);
+		}
+
+		void operator()( int (* hypre_set_func)(HYPRE_Solver, int**) & , int** & value){
+			Ifpack_obj.SetParameter(solver_preconditioner_selection,hypre_set_func,value);
+		}
+
+		template <typename T, typename U>
+		void operator()(T & func, U & value){
+			// It will be an error to have any other combination of items
+			(void) func;
+			(void) value;
+		}
+
+	private:
+		Ifpack_Hypre & Ifpack_obj;
+		const Hypre_Chooser solver_preconditioner_selection;
+	};
+
+};
+
+class SolverBoomerAMG{
+public:
+	void solve(LA::SparseMatrix & system_matrix,
+			   LA::Vector & right_hand_side,
+			   LA::Vector &solution);
+};
 
 } // Close namespace TrilinosWrappers
 DEAL_II_NAMESPACE_CLOSE
