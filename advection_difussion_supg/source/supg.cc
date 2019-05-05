137be43aa8f64fa66bfd918884de267c54e8ed77
jthano <josh.hanophy@gmail.com>
2019-05-04 22:37:46 -0500

    Cleaning up the SUPG example

--- advection_difussion_supg/source/supg.cc
+++ advection_difussion_supg/source/supg.cc
@@ -21,8 +21,11 @@
 #include <deal.II/base/quadrature_lib.h>
 #include <deal.II/base/function.h>
 #include <deal.II/base/timer.h>
+#include <deal.II/base/geometry_info.h>
 
 #include <deal.II/lac/generic_linear_algebra.h>
+
+#include <deal.II/grid/grid_tools.h>
 
 namespace LA
 {
@@ -73,18 +76,22 @@ namespace LA
 using namespace dealii;
 
 
-class Step3
+class Advection_Diffusion
 {
 public:
-  Step3();
+
+  enum boundary_condition_type {HOMOGENEOUS_DIRICHLET, HOMGENEOUS_NATURAL};
+  enum solver_option {DIRECT, AIR_AMG, CLASSIC_AMG};
+
+  Advection_Diffusion(boundary_condition_type bc_type, solver_option solver_type, bool stabilize);
 
   void run();
-
 
 private:
   void make_grid();
   void setup_system();
   void assemble_system();
+  void assemble_system_stabilized();
   void solve();
   void output_results() const;
 
@@ -106,10 +113,18 @@ private:
   ConditionalOStream pcout;
   TimerOutput        computing_timer;
 
+  Point<2> velocity;
+  const double speed = 100;
+  const double nu;
+
+  boundary_condition_type bc_type;
+  solver_option solver_type;
+  bool stabilize;
+
 };
 
 
-Step3::Step3()
+Advection_Diffusion::Advection_Diffusion(boundary_condition_type bc_type, solver_option solver_type, bool stabilize)
 : mpi_communicator(MPI_COMM_WORLD)
 , triangulation(mpi_communicator,
                 typename Triangulation<2>::MeshSmoothing(
@@ -123,26 +138,49 @@ Step3::Step3()
                   pcout,
                   TimerOutput::summary,
                   TimerOutput::wall_times)
-{}
+,	nu(1.0)
+, bc_type(bc_type)
+, solver_type(solver_type)
+, stabilize(stabilize)
+{
+	  velocity(0) = pow(2.0,0.5)/2.0;//0.15;//pow(2.0,0.5)/2.0;
+	  velocity(1) = pow(2.0,0.5)/2.0;//0.9886859966642595;//velocity(0);
+
+	  velocity(0) *= speed;
+	  velocity(1) *= speed;
+}
 
 
 
-void Step3::make_grid()
+void Advection_Diffusion::make_grid()
 {
   GridGenerator::hyper_cube(triangulation, -1, 1);
-  triangulation.refine_global(5);
+  triangulation.refine_global(1);
 
-  //GridTools::distort_random(0.3, triangulation);
+  Vector<float> dummy_error;
 
-  std::cout << "Number of active cells: " << triangulation.n_active_cells()
+  //9
+  for(int i=0;i<4;++i){
+  	dummy_error.reinit(triangulation.n_active_cells());
+  	dummy_error = 1.0;
+  	parallel::distributed::GridRefinement::refine_and_coarsen_fixed_number(triangulation,dummy_error,1.0,0.0);
+  	triangulation.execute_coarsening_and_refinement();
+  }
+
+  //GridTools::distort_random(0.2, triangulation);
+
+  pcout << "Number of active cells: " << triangulation.n_active_cells()
             << std::endl;
 }
 
 
 
 
-void Step3::setup_system()
+void Advection_Diffusion::setup_system()
 {
+
+
+
     TimerOutput::Scope t(computing_timer, "setup");
 
     dof_handler.distribute_dofs(fe);
@@ -157,7 +195,53 @@ void Step3::setup_system()
 
     constraints.clear();
     constraints.reinit(locally_relevant_dofs);
-   // DoFTools::make_hanging_node_constraints(dof_handler, constraints);
+
+
+    /**
+     * Here the ability to set natural boundary conditions are added. That is, the inlfow
+     * boundary condition is treated as Dirichlet and the outflow boundary is treated by specifying
+     * a diffusive flux. For simplicity, both values are currently hardcoded as 0.0
+     */
+    if (bc_type == HOMGENEOUS_NATURAL)
+    {
+		QGauss<1> face_quadrature_formula(1);
+
+		FEFaceValues<2> fe_face_values (fe, face_quadrature_formula,
+										  update_quadrature_points  |
+										  update_normal_vectors );
+
+		/**
+		 * Loop through all relevant cells and find outflow boundaries and mark these
+		 * with a boundary_id other than 0. This will prevent interpolate_boundary_values
+		 * called after this loop from applying Dirichlet boundary conditions at these
+		 * faces.
+		 */
+
+		for (const auto &cell : dof_handler.active_cell_iterators())
+		  {
+			if (cell->is_locally_owned() || cell->is_ghost())
+			  {
+
+				for (unsigned int face_number = 0;
+					 face_number < GeometryInfo<2>::faces_per_cell;
+					 ++face_number)
+				  if (cell->face(face_number)->at_boundary() )
+					{
+					  fe_face_values.reinit(cell, face_number);
+
+					  double beta = velocity*fe_face_values.normal_vector(0);
+
+					  if (beta>=0.0){
+						  cell->face(face_number)->set_boundary_id(1);
+					  }
+
+					}
+
+			  }
+		  }
+
+    }
+
     VectorTools::interpolate_boundary_values(dof_handler,
                                              0,
                                              Functions::ZeroFunction<2>(),
@@ -177,20 +261,94 @@ void Step3::setup_system()
                          locally_owned_dofs,
                          dsp,
                          mpi_communicator);
+}
+
+/**
+ * This function is essentially the same as that from step 40 except that an advection
+ * term has been added. Additionally, linear elements are being used.
+ */
+void Advection_Diffusion::assemble_system(){
+
+	QGauss<2> quadrature_formula(2);
+
+	FEValues<2> fe_values(fe,
+	                      quadrature_formula,
+	                      update_values | update_gradients | update_JxW_values);
+
+	const unsigned int dofs_per_cell = fe.dofs_per_cell;
+	const unsigned int n_q_points    = quadrature_formula.size();
+
+	FullMatrix<double> cell_matrix(dofs_per_cell, dofs_per_cell);
+	Vector<double>     cell_rhs(dofs_per_cell);
+
+	std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell);
+
+	/**
+	 * The assembly processes is the same from step 40 except and advection term
+	 * is added and the option for a variable diffusion coefficient nu is added
+	 */
+	  for (const auto &cell : dof_handler.active_cell_iterators())
+	    {
+	      if (cell->is_locally_owned())
+	        {
+
+			  cell_matrix = 0;
+			  cell_rhs    = 0;
+
+			  fe_values.reinit(cell);
+
+			  for (unsigned int q_index = 0; q_index < n_q_points; ++q_index)
+				{
+				  for (unsigned int i = 0; i < dofs_per_cell; ++i)
+					for (unsigned int j = 0; j < dofs_per_cell; ++j){
+					  cell_matrix(i, j) +=
+						nu*(fe_values.shape_grad(i, q_index) *
+						 fe_values.shape_grad(j, q_index) *
+						 fe_values.JxW(q_index));
+
+					  cell_matrix(i, j) +=
+						(velocity * fe_values.shape_grad(j, q_index)) *
+						 fe_values.shape_value(i, q_index) *
+						 fe_values.JxW(q_index);
+
+					}
+				  for (unsigned int i = 0; i < dofs_per_cell; ++i)
+				  {
+					cell_rhs(i) += (fe_values.shape_value(i, q_index) *
+									1.0 * fe_values.JxW(q_index));
+				  }
+				}
+
+			  cell->get_dof_indices(local_dof_indices);
+
+	          cell->get_dof_indices(local_dof_indices);
+	          constraints.distribute_local_to_global(cell_matrix,
+	                                                 cell_rhs,
+	                                                 local_dof_indices,
+	                                                 system_matrix,
+	                                                 system_rhs);
+	        }
+	    }
+
+	  system_matrix.compress(VectorOperation::add);
+	  system_rhs.compress(VectorOperation::add);
 
 
 }
-
-
-
-void Step3::assemble_system()
+/**
+ * This function implements an SUPG discretization. The specifics of the stabilization
+ * are presented in the report.
+ */
+void Advection_Diffusion::assemble_system_stabilized()
 {
   bool stabilize = true;
 
   QGauss<2> quadrature_formula(2);
+
   FEValues<2> fe_values(fe,
                         quadrature_formula,
                         update_values | update_gradients | update_JxW_values);
+
 
   const unsigned int dofs_per_cell = fe.dofs_per_cell;
   const unsigned int n_q_points    = quadrature_formula.size();
@@ -200,15 +358,6 @@ void Step3::assemble_system()
 
   std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell);
 
-  Point<2> velocity;
-  velocity(0) = pow(2.0,0.5)/2.0;
-  velocity(1) = velocity(0);
-  const double speed = 100;
-  velocity(0) *= speed;
-  velocity(1) *= speed;
-  const double nu = 1.0;
-
-  //auto mapping = fe_values.get_mapping();
 
   for (const auto &cell : dof_handler.active_cell_iterators())
     {
@@ -251,25 +400,23 @@ void Step3::assemble_system()
 		  //
 		  double tau = (etta_bar*a_etta*h_etta + xi_bar*a_xi*h_xi)/2.0/pow(velocity.norm(),2);
 		  //
-
-		  //
 		  for (unsigned int q_index = 0; q_index < n_q_points; ++q_index)
 			{
 			  for (unsigned int i = 0; i < dofs_per_cell; ++i)
 				for (unsigned int j = 0; j < dofs_per_cell; ++j){
 				  cell_matrix(i, j) +=
-					(fe_values.shape_grad(i, q_index) *
+					nu*(fe_values.shape_grad(i, q_index) *
 					 fe_values.shape_grad(j, q_index) *
 					 fe_values.JxW(q_index));
 
 				  cell_matrix(i, j) +=
-					(velocity * fe_values.shape_grad(i, q_index) *
-					 fe_values.shape_value(j, q_index) *
+					(velocity * fe_values.shape_grad(j, q_index) *
+					 fe_values.shape_value(i, q_index) *
 					 fe_values.JxW(q_index));
 
 				  cell_matrix(i,j) += (fe_values.shape_grad(i,q_index)*
-						  velocity)*tau*(fe_values.shape_grad(j,q_index)*velocity) *
-						  fe_values.JxW(q_index);
+						  velocity)*tau*(fe_values.shape_grad(j,q_index)*velocity)
+						  *fe_values.JxW(q_index);
 
 				}
 			  for (unsigned int i = 0; i < dofs_per_cell; ++i)
@@ -279,6 +426,8 @@ void Step3::assemble_system()
 				cell_rhs(i) += (fe_values.shape_grad(i,q_index)*velocity)*tau*fe_values.JxW(q_index);
 			  }
 			}
+
+
 		  cell->get_dof_indices(local_dof_indices);
 
           cell->get_dof_indices(local_dof_indices);
@@ -297,19 +446,34 @@ void Step3::assemble_system()
 
 
 
-void Step3::solve()
+void Advection_Diffusion::solve()
 {
     TimerOutput::Scope t(computing_timer, "solve");
 
     LA::MPI::Vector    completely_distributed_solution(locally_owned_dofs,
                                                     mpi_communicator);
 
-	TrilinosWrappers::BoomerAMG_Parameters AMG_parameters(TrilinosWrappers::BoomerAMG_Parameters::AIR_AMG);
-	//AMG_parameters.set_parameter_value("distance_R",1.0);
+    if (solver_type == AIR_AMG){
+    	TrilinosWrappers::BoomerAMGParameters AMG_parameters(TrilinosWrappers::BoomerAMGParameters::AIR_AMG, Hypre_Chooser::Solver);
+        /**
+         * Demonstrate changing a parameter value
+         */
+    	AMG_parameters.set_parameter_value("relax_type",3);
+    	TrilinosWrappers::SolverBoomerAMG AMG_solver(AMG_parameters);
+    	AMG_solver.solve(system_matrix, system_rhs, completely_distributed_solution);
+    	AMG_solver.solve(system_matrix, system_rhs, completely_distributed_solution);
 
-	TrilinosWrappers::SolverBoomerAMG AMG_solver(AMG_parameters);
+    }else if (solver_type == CLASSIC_AMG){
+    	TrilinosWrappers::BoomerAMGParameters AMG_parameters(TrilinosWrappers::BoomerAMGParameters::CLASSICAL_AMG, Hypre_Chooser::Solver);
+    	TrilinosWrappers::SolverBoomerAMG AMG_solver(AMG_parameters);
+    	AMG_solver.solve(system_matrix, system_rhs, completely_distributed_solution);
+    } else{
+        SolverControl solver_control(3000,1e-6);
+    	TrilinosWrappers::SolverDirect Solv(solver_control);
+    	Solv.initialize(system_matrix);
+    	Solv.solve(completely_distributed_solution,system_rhs);
 
-	AMG_solver.solve(system_matrix, system_rhs, completely_distributed_solution);
+    }
 
     constraints.distribute(completely_distributed_solution);
 
@@ -319,7 +483,7 @@ void Step3::solve()
 
 
 
-void Step3::output_results() const
+void Advection_Diffusion::output_results() const
 {
 	int cycle=1;
 
@@ -358,11 +522,12 @@ void Step3::output_results() const
 
 
 
-void Step3::run()
+void Advection_Diffusion::run()
 {
   make_grid();
   setup_system();
-  assemble_system();
+  assemble_system_stabilized();
+  //assemble_system();
   solve();
   output_results();
 }
@@ -375,7 +540,7 @@ int main(int argc, char *argv[])
 
   deallog.depth_console(2);
 
-  Step3 laplace_problem;
+  Advection_Diffusion laplace_problem(Advection_Diffusion::HOMOGENEOUS_DIRICHLET,Advection_Diffusion::DIRECT, true );
   laplace_problem.run();
 
   return 0;
